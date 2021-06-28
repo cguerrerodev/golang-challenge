1c69e62a4b8ae89e303ea044613c02efecd97b94
cguerrero-dev <carlos.guerrero@bairesdev.com>
2021-06-28 13:44:43 -0500

    Making GetPricesFor parallelized

--- cache.go
+++ cache.go
@@ -61,13 +61,37 @@ func (c *TransparentCache) GetPriceFor(itemCode string) (float64, error) {
 // If any of the operations returns an error, it should return an error as well
 func (c *TransparentCache) GetPricesFor(itemCodes ...string) ([]float64, error) {
 	results := []float64{}
+	ch := make(chan result, 10)
+
 	for _, itemCode := range itemCodes {
 		// TODO: parallelize this, it can be optimized to not make the calls to the external service sequentially
-		price, err := c.GetPriceFor(itemCode)
-		if err != nil {
-			return []float64{}, err
-		}
-		results = append(results, price)
+		go c.GetPriceForAsc(itemCode, ch)
 	}
+
+	close(ch)
+
+	for r := range ch {
+
+		if r.err != nil {
+			return []float64{}, r.err
+		}
+		results = append(results, r.price)
+	}
+
 	return results, nil
+}
+
+type result struct {
+	price float64
+	err   error
+}
+
+func (c *TransparentCache) GetPriceForAsc(itemCode string, ch chan result) {
+
+	price, err := c.GetPriceFor(itemCode)
+	ch <- result{
+		price: price,
+		err:   err,
+	}
+
 }
